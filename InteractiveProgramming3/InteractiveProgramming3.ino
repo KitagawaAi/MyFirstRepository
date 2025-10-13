337accdf6c43598b21653e0c209e527e14ac30a1
KitagawaAi <mlu855220@gmail.com>
2025-10-13 21:41:46 +0800

    HW1_Shadow Lamp

--- InteractiveProgramming3/InteractiveProgramming3.ino
+++ InteractiveProgramming3/InteractiveProgramming3.ino
@@ -5,7 +5,6 @@ const int BledPin = 5;
 
 int buttonState = 0;
 int ledcolor = 0;
-
 bool ButtonPressed = false;
 
 void setup() {
@@ -18,63 +17,54 @@ void setup() {
 void loop() {
   buttonState = digitalRead(buttonPin);
 
-  if(buttonState == HIGH && !ButtonPressed){
-    ledcolor = ledcolor + 1;
-    ButtonPressed =true;
-    //delay(100);
+  if (buttonState == HIGH && !ButtonPressed) {
+    ledcolor++;
+    ButtonPressed = true;
+    colorTransitionEffect();
   }
 
-  if(buttonState == LOW && ButtonPressed){
+  if (buttonState == LOW && ButtonPressed) {
     ButtonPressed = false;
   }
 
-  if(ledcolor == 0){
-  digitalWrite(RledPin, HIGH);
-  digitalWrite(GledPin, HIGH);
-  digitalWrite(BledPin, HIGH); 
+  if (ledcolor > 7) ledcolor = 0;
+  setColorByMode(ledcolor);
+}
+
+void setColorByMode(int mode) {
+  switch (mode) {
+    case 0: setColor(1, 1, 1); break; // off / white dim
+    case 1: setColor(0, 1, 1); break; // red
+    case 2: setColor(1, 0, 1); break; // green
+    case 3: setColor(1, 1, 0); break; // blue
+    case 4: setColor(0, 0, 1); break; // yellow
+    case 5: setColor(0, 1, 0); break; // purple
+    case 6: setColor(1, 0, 0); break; // cyan
+    case 7: rainbowEffect();  break;  // 彩虹閃爍模式
   }
-  else if(ledcolor == 1){
-  //RED
-  digitalWrite(RledPin, LOW);
-  digitalWrite(GledPin, HIGH);
-  digitalWrite(BledPin, HIGH); 
+}
+
+void setColor(int r, int g, int b) {
+  digitalWrite(RledPin, r);
+  digitalWrite(GledPin, g);
+  digitalWrite(BledPin, b);
+}
+
+void rainbowEffect() {
+  for (int i = 0; i < 3; i++) {
+    setColor(0, 1, 1); delay(150); // red
+    setColor(1, 0, 1); delay(150); // green
+    setColor(1, 1, 0); delay(150); // blue
+    setColor(0, 0, 1); delay(150); // yellow
+    setColor(0, 1, 0); delay(150); // purple
+    setColor(1, 0, 0); delay(150); // cyan
   }
-  else if(ledcolor == 2){
-  //GREEN
-  digitalWrite(RledPin, HIGH);
-  digitalWrite(GledPin, LOW);
-  digitalWrite(BledPin, HIGH); 
-  }
-  else if(ledcolor == 3){
-  //BLUE
-  digitalWrite(RledPin, HIGH);
-  digitalWrite(GledPin, HIGH);
-  digitalWrite(BledPin, LOW); 
-  }
-  else if(ledcolor == 4){
-  digitalWrite(RledPin, LOW);
-  digitalWrite(GledPin, LOW);
-  digitalWrite(BledPin, HIGH); 
-  }
-  //purple
-  else if(ledcolor == 5){
-  digitalWrite(RledPin, LOW);
-  digitalWrite(GledPin, HIGH);
-  digitalWrite(BledPin, LOW); 
-  }
-  //cyan
-  else if(ledcolor == 6){
-  digitalWrite(RledPin, HIGH);
-  digitalWrite(GledPin, LOW);
-  digitalWrite(BledPin, LOW); 
-  }
-  //white
-  else if(ledcolor == 7){
-  digitalWrite(RledPin, LOW);
-  digitalWrite(GledPin, LOW);
-  digitalWrite(BledPin, LOW); 
-  }
-  else if(ledcolor == 8){
-  ledcolor = 0;
+  setColor(0, 0, 0); // 最後回白光
+}
+
+void colorTransitionEffect() {
+  for (int i = 0; i < 3; i++) {
+    setColor(1, 1, 1); delay(100);
+    setColor(0, 0, 0); delay(100);
   }
 }
